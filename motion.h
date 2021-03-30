0cd086152bb9d80d39d007bc117a25c0aba0c0a7
Bryan Zheng <ashuh223@gmail.com>
2021-03-31 02:23:06 +0800

    Add motion.h

---
+++ motion.h
@@ -0,0 +1,16 @@
+#ifndef __MOTION_H__
+#define __MOTION_H__
+
+struct Position {
+    int x;
+    int y;
+};
+
+enum DIRECTION {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    NONE
+};
+#endif // __MOTION_H__
