d70dc8c14a107eb129ec628d2adcda2944cf275d
selvavignesh-2206 <b.selvavignesh@gmail.com>
2021-04-05 21:04:18 +0800

    Testing

--- snake_planner/main.c
+++ snake_planner/main.c
@@ -5,12 +5,13 @@
 #include <time.h>
 
 
-int xMovement (int diffX, int snakeX, int snakeY, int xCood[]) {
+int xMovement (int diffX, int snakeX, int snakeY, int xCood[], int num) {
     if (diffX > 0) {
         // Means the mouse is on the left of the snake
         for (int i=1; i<=diffX; ++i) {
             snakeX -= 1;
             printf("Snake pose: %i , %i\n", snakeX, snakeY);
+            //arrayX(xCood, num);
         }
 
     } else if (diffX < 0) {
@@ -27,7 +28,7 @@ int xMovement (int diffX, int snakeX, int snakeY, int xCood[]) {
     return snakeX;
 }
 
-int yMovement(int diffY, int snakeX, int snakeY,int yCood[]) {
+int yMovement(int diffY, int snakeX, int snakeY,int yCood[], int num) {
     if (diffY > 0) {
         // Means the mouse is on the top of the snake 
         for (int i=1; i<=diffY; ++i) {
@@ -48,16 +49,6 @@ int yMovement(int diffY, int snakeX, int snakeY,int yCood[]) {
     return snakeY;
 }
 
-void delay(int second){
-
-    int milsec = 1000 * second;
-
-    clock_t startTime = clock();
-
-    while(clock() < (startTime + milsec));
-
-}
-
 // To generate the random starting position of the mouse and end goal of the mouse
 void randLimit(int lowerLimitX, int lowerLimitY, int upperLimitX, int upperLimitY,  int* goalX, int* goalY) {
     
@@ -67,31 +58,38 @@ void randLimit(int lowerLimitX, int lowerLimitY, int upperLimitX, int upperLimit
     *goalY = rand()%(upperLimitY + 1 - lowerLimitY) + lowerLimitY;  
 }
 
-int arrayX(int x, int xCood[]) {
+/*
+int arrayX(int x, int xCood[], int num) {
     
-    /*
+    
     int temp = xCood[num-1];
     xCood[num-1] = x;
 
     int temp2;
     for (int i=num-2; i>=0; ++i) {
+        temp2 = xCood[i];
     }
-
-    */
 
 }
 
-
+*/
+int dist(int snakePose,int mousePose) {
+    
+    int diff = snakePose - mousePose;
+    
+    return diff;
+}
 
 int main() {
     
     int goalX, goalY;
     int mouseXPose, mouseYPose;
 
+    // Genereation of the start & end pt
     randLimit(0,6,4,10,&goalX, &goalY);
     randLimit(7,0,10,4,&mouseXPose, &mouseYPose);
 
-    int snakeSize = 5;
+    int num = 5;
 
     int snakeX = 5;
     int snakeY = 5;
@@ -99,10 +97,8 @@ int main() {
     printf("Starting pose; X: %i , Y: %i\n", mouseXPose, mouseYPose);
     printf("End goal; X: %i , Y: %i\n", goalX, goalY);
 
-    //delay(10);
-
-    int diffX = snakeX - mouseXPose;
-    int diffY = snakeY - mouseYPose;
+    int diffX = dist(snakeX, mouseXPose);
+    int diffY = dist(snakeY, mouseYPose);
 
     printf("diffX: %i, diffY: %i\n", diffX, diffY); 
 
@@ -111,13 +107,13 @@ int main() {
     int yCood[3] = {5,5,5};
 
     if ((diffX > diffY) || (diffX == diffY)) {
-       snakeX = xMovement(diffX, snakeX, snakeY, xCood);
-       snakeY = yMovement(diffY, snakeX, snakeY, yCood);
+       snakeX = xMovement(diffX, snakeX, snakeY, xCood, num);
+       snakeY = yMovement(diffY, snakeX, snakeY, yCood, num);
        printf("Final Snake Pose; X: %i, Y: %i\n" , snakeX, snakeY );
 
     } else if (diffX < diffY) {
-       snakeY = yMovement(diffY, snakeX, snakeY, yCood);
-       snakeX = xMovement(diffX, snakeX, snakeY, xCood);
+       snakeY = yMovement(diffY, snakeX, snakeY, yCood, num);
+       snakeX = xMovement(diffX, snakeX, snakeY, xCood, num);
        printf("Final Snake Pose; X: %i, Y: %i\n" , snakeX, snakeY );       
     }
 
